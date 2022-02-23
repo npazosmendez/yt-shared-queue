0093db009fbc419003fc56c9e00e475384abe271
Nicolás Pazos-Méndez <npazosmendez@gmail.com>
2022-02-23 19:38:03 -0300

    Quita línea redundante

--- src/public/javascripts/player.ts
+++ src/public/javascripts/player.ts
@@ -4,7 +4,6 @@ var playerState : YT.PlayerState;
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
-tag.setAttribute("allow", "autoplay");
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
 
